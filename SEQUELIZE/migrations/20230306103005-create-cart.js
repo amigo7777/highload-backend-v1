acc0bacb1ac8c660b047df5123255a82f7810013
amigo7777 <76115359+amigo7777@users.noreply.github.com>
2023-03-06 13:34:14 +0300

    first commit

---
+++ SEQUELIZE/migrations/20230306103005-create-cart.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      list: {
+        type: Sequelize.STRING
+      },
+      sumprice: {
+        type: Sequelize.INTEGER
+      },
+      buyer: {
+        type: Sequelize.STRING
+      },
+      customers_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('carts');
+  }
+};
